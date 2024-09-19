008c91ddf8417cf3031e651de0cc684885269a56
Gerardo Mendoza Munoz <gdoza@Gerardos-MacBook-Air.local>
2024-09-19 15:16:11 -0600

    review logic

--- backend/api/movies.route.js
+++ backend/api/movies.route.js
@@ -1,5 +1,12 @@
 import express from 'express'
 import MoviesController from './movies.controller.js'
+import ReviewsController from './reviews.controller.js'
+
 const router = express.Router()
-router.route('/').get((req,res) => res.send(MoviesController.apiGetMovies))
+router.route('/').get(MoviesController.apiGetMovies)
+router
+	.rout("/review")
+	.post(ReviewsController.apiPostReview)
+	.put(ReviewsController.apiUpdateReview)
+	.delete(ReviewsController.apiDeleteReview)
 export default router
